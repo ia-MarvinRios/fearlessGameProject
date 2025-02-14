8b4c3415cde5620059ee8c58fd090e09ee50d99d
Marvin Rios <ia.marvinrios@ufg.edu.sv>
2025-02-14 13:54:57 -0600

    SceneManagementAdded

--- Medium/Assets/Scripts/MainMenuManager.cs
+++ Medium/Assets/Scripts/MainMenuManager.cs
@@ -1,17 +1,49 @@
+using System;
+using TMPro;
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class MainMenuManager : MonoBehaviour
 {
+    public TMP_Dropdown scenesDropdown;
+    private int sceneIndex;
+
     void Awake()
     {
         Time.timeScale = 1f;
         Debug.Log("Tiempo reanudado: " + Time.timeScale);
     }
+    void Start()
+    {
+        int selectedIndex = scenesDropdown.value;
+        string selectedText = scenesDropdown.options[selectedIndex].text;
+
+        Debug.Log("Índice: " + selectedIndex + " Texto: " + selectedText);
+    }
 
     public void PlayGame()
     {
-        SceneManager.LoadScene("Rios_witchLevel");
+        int sceneIndex = scenesDropdown.value;
+        switch (sceneIndex) {
+            case 0:
+                SceneManager.LoadScene("rafaVis");
+                break;
+
+            case 1:
+                SceneManager.LoadScene("Rios_witchLevel");
+                break;
+
+            case 2:
+                SceneManager.LoadScene("Rios_witchLevel");
+                break;
+
+            default:
+                SceneManager.LoadScene("Rios_witchLevel");
+                break;
+
+        }
     }
 
     public void ExitGame()
