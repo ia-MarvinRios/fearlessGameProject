4471b51eb2fee20b74fff076eeae0b2e05a132fd
Marvin Rios <ia.marvinrios@ufg.edu.sv>
2025-04-28 18:37:30 -0600

    DeletedFiles

--- Assets/Scripts/NPCs/Dialogue Data.cs
+++
@@ -1,9 +0,0 @@
-using UnityEngine;
-
-[CreateAssetMenu]
-public class DialogueData : ScriptableObject
-{
-    [SerializeField] private Dialogue[] dialogues;
-
-    public Dialogue[] Dialogues { get{ return dialogues; } }
-}
